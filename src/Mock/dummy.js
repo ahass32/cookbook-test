ce0bc1e21b47fffb8555aabe56624313aec76d6e
Ahmad Hassouneh <a.hassouneh32@gmail.com>
2022-06-09 18:31:35 +0400

    feat : dummy data

---
+++ src/Mock/dummy.js
@@ -0,0 +1,30 @@
+export const Recipes = [
+  {
+    author: "Jim",
+    name: "Chicken Curry",
+    description: "Delicious spicy chicken curry",
+    image: "https://images.media-allrecipes.com/userphotos/9158155.jpg",
+    ingredients: [" souce, parsly, chiken"],
+    price: "140",
+  },
+  {
+    author: "Aravind",
+    name: "Hamburger",
+    description: "Juicy burger with toppings and a soft bun",
+    image:
+      "https://assets.epicurious.com/photos/57c5c6d9cf9e9ad43de2d96e/master/w_1280,c_limit/the-ultimate-hamburger.jpg",
+    ingredients: [" eggs, bacon, meat"],
+    price: "100",
+  },
+
+  {
+    author: "Lpadola",
+    name: "chicken Shawerma",
+    description:
+      "a sandwich especially of sliced lamb or chicken, vegetables, and often tahini wrapped in pita bread.",
+    image:
+      "https://www.thespruceeats.com/thmb/qWxT_QU0PlqphVPfgMtfQjSKPO0=/1867x1867/smart/filters:no_upscale()/chicken-shawarma-138740297-58260f943df78c6f6acabb5b.jpg",
+    ingredients: [" bread, pickles, chiken"],
+    price: "80",
+  },
+];
